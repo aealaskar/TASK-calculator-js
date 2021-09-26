7e0bf7878fee839aacea31e8c4f657e86cc584c0
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-09-26 20:20:43 +0300

    Part One Done

--- index.js
+++ index.js
@@ -7,9 +7,47 @@
  * If you click on button +, the text will be "+"
  * ,... and so on
  */
+let firstNumber = []
+let secondNumer = []
+const sum = Number.firstNumber + Number.secondNumer
+const sub = Number.firstNumber - Number.secondNumer
+const div = Number.firstNumber / Number.secondNumer
+const multi = Number.firstNumber * Number.secondNumer
+const nums = [1,2,3,4,5,6,7,8,9,0,"."]
 function buttonClick(text) {
+  
+ /*  nums.forEach(num => {
+    num !== text
+    console.log('working')
+  }); */
+/* 
+  if (text !== nums.forEach(num => num !== text)){
+    console.log('working')
+  } */
+
+  if (text === "x" || text === "-"|| text === "+"||text === "÷"){
+    
+    return;
+  }
+
+  if (text === "x" || text === "-"|| text === "+"||text === "÷" ) {
+    secondNumer 
+    printOnConsole(secondNumer.reduce((a, b) => a.toString() + b.toString()))
+    console.log(secondNumer)
+  }
+
   console.log("Clicking", text);
-  // Write your code here
+  if (text === "AC") {
+    firstNumber =[]
+    printOnConsole('0')
+    return;
+  }
+  if (firstNumber[0] === 0){
+    firstNumber =[]
+    return;
+  }
+  firstNumber.push(text)
+  printOnConsole(firstNumber.reduce((a, b) => a.toString() + b.toString()))
 }
 
 /** Supporting functions
